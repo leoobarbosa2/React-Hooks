e6ebb38efd0afdcf73c67ee3eb937be112b3354e
Leonardo Barbosa <leonardo.pb@icloud.com>
2019-11-21 09:06:28 -0300

    Hook UseEffect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
-  function handleAdd() {
-    setTech([...tech, newTech]);
+  function handleAddTech() {
+    setTechs([...techs, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
       <ul>
-        {tech.map(t => (
-          <li key={t}>{t}</li>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
         ))}
+        <input
+          value={newTech}
+          onChange={e => setNewTech(e.target.value)}
+          type="text"
+        />
+        <button onClick={handleAddTech} type="button">
+          Adicionar
+        </button>
       </ul>
-      <input
-        value={newTech}
-        onChange={e => setNewTech(e.target.value)}
-        type="text"
-      />
-      <button type="button" onClick={handleAdd}>
-        Adicionar
-      </button>
     </>
   );
 }
